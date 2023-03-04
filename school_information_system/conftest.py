122ec7972a3171bc96e08b0c2737aba7939c46d9
Eivind Teig <eivind.teig@gmail.com>
2023-03-04 22:47:11 +0100

    Initial commit

---
+++ school_information_system/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from django.test import RequestFactory
+
+from school_information_system.users.models import User
+from school_information_system.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
+
+
+@pytest.fixture
+def request_factory() -> RequestFactory:
+    return RequestFactory()
